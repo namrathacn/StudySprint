34ec81cfd7e293aa3df77f9b72f736c70135c75c
Namratha C N <namrathagowda2628@gmail.com>
2026-07-17 09:28:20 +0530

    Fix Firebase Admin

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -4,39 +4,22 @@ const { getAuth } = require("firebase-admin/auth");
 
 require("dotenv").config();
 
-
-
-if (getApps().length === 0) {
-
-    initializeApp({
-
-        credential: cert({
-
-            projectId: process.env.FIREBASE_PROJECT_ID,
-
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-
-            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
-
-        })
-
-    });
-
+if (!getApps().length) {
+  initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    }),
+  });
 }
-
-
 
 console.log("Firebase Admin Connected Successfully");
 
-
-
 const db = getFirestore();
-
 const auth = getAuth();
 
-
-
 module.exports = {
-    db,
-    auth
+  db,
+  auth,
 };
